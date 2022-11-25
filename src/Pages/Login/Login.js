66c8b9d1ce628ee98d917a283a4465cd1c883994
Upoma21801~ <upoma.cdsr@gmail.com>
2022-11-26 02:45:37 +0600

    login and register added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,85 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
+import useToken from '../../Hooks/useToken';
+// import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { signIn, providerLogin } = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
+    const [loginUserEmail, setLoginUserEmail] = useState('');
+    const [token] = useToken(loginUserEmail);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
+    if (token) {
+        navigate(from, { replace: true });
+    }
+
+    const googleProvider = new GoogleAuthProvider();
+
+    // console.log(from);
+    const handleLogin = data => {
+        console.log(data);
+        setLoginError('');
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setLoginUserEmail(data.email);
+            })
+            .catch(error => {
+                console.log(error.message)
+                setLoginError(error.message);
+            });
+    }
+
+    const handleGoogleSignIn = () => {
+        providerLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate(from, { replace: true });
+            })
+            .catch(error => console.error(error))
+    }
     return (
-        <div>
-            
+        <div className='h-[800px] flex justify-center items-center border  '>
+            <div className='w-96 p-7 rounded-2xl shadow-xl'>
+                <h2 className='text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full min-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="email"
+                            {...register("email", {
+                                required: "Email Address is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full min-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        <label className="label"> <span className="label-text">Forget Password?</span></label>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                    </div>
+                    <input className='btn btn-accent w-full text-white' value="Login" type="submit" />
+                    <div>
+                        {loginError && <p className='text-red-600'>{loginError}</p>}
+                    </div>
+                </form>
+                <p className='my-3'>New to Doctors Portal <Link className='text-info' to="/signup">Create new Account</Link></p>
+                <div className="divider">OR</div>
+                <button onClick={handleGoogleSignIn} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
+import useToken from '../../Hooks/useToken';
 
 const SignUp = () => {
+    const { createUser, updateUser } = useContext(AuthContext)
+    const [signUpError, setSignUpError] = useState();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [createdUserEmail, setCreatedUserEmail] = useState('');
+    const [token] = useToken(createdUserEmail);
+    const navigate = useNavigate();
+    if (token) {
+        navigate('/');
+    }
+    const handleSignUp = data => {
+        setSignUpError('');
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                toast('Registration successful')
+                const userInfo = {
+                    displayName: data.name,
+                }
+                updateUser(userInfo)
+                    .then(() => {
+                        saveUser(data.name, data.email)
+                    })
+
+                    .catch(error => console.error(error))
+            })
+            .catch(error => {
+                console.error(error);
+                setSignUpError(error.message);
+            })
+    }
+
+    const saveUser = (name, email) => {
+        const user = { name, email };
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json()
+                .then(data => {
+                    setCreatedUserEmail(email)
+                }))
+    }
     return (
-        <div>
-            
+        <div className='h-[800px] flex justify-center items-center border  '>
+            <div className='w-96 p-7 rounded-2xl shadow-xl'>
+                <h2 className='text-xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full min-w-xs">
+                        <label className="label"> <span className="label-text">Name</span></label>
+                        <input type="text" {...register("name", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full min-w-xs" />
+                        {errors.name && <p className='text-red-600 py-3'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full min-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="email"{...register('email', {
+                            required: "Email is required"
+                        })} className="input input-bordered w-full min-w-xs" />
+                        {errors.email && <p className='text-red-600 py-3'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full min-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password"{...register('password', {
+                            required: "Password is required",
+                            minLength: { value: 6, message: "Password should be minimum 6 character long" },
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])./, message: "Password must be included with minimum 1 uppercase, 1 lowercase, 1 digit and 1 special character" }
+                        })}
+                            className="input input-bordered w-full min-w-xs mb-5" />
+                        {errors.password && <p className='text-red-600 my-2'>{errors.password.message}</p>}
+                    </div>
+                    <input className='btn btn-accent w-full text-white' value="Sign Up" type="submit" />
+                    {signUpError && <p className='text-red-600 py-3'>{signUpError}</p>}
+                </form>
+                <p className='my-3'>Already have an account? <Link className='text-info' to="/login">Login Now</Link></p>
+            </div>
         </div>
     );
 };
